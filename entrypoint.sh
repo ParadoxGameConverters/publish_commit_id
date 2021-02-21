dbfd78bab939516a54583b68c101dc68fdbd79ea
Stephen May <idhrendur@hotmail.com>
2021-02-21 14:09:38 -0800

    Initial creation of action

---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+set -x
+
+if [ -z "$INPUT_DESTINATION_BRANCH" ]
+then
+  INPUT_DESTINATION_BRANCH=master
+fi
+OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
+
+CLONE_DIR=$(mktemp -d)
+
+echo "Cloning destination git repository"
+git config --global user.email "$INPUT_USER_EMAIL"
+git config --global user.name "$INPUT_USER_NAME"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com:ParadoxGameConverters/ParadoxGameConverters.github.io.git" "$CLONE_DIR"
+
+if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+then
+  git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
+  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
+fi
+
+echo "Copying contents to git repo"
+mkdir -p $CLONE_DIR/commit_ids
+git rev-parse HEAD > "$CLONE_DIR/commit_ids/$INPUT_DESTINATION_FILE"
+cd "$CLONE_DIR"
+
+if [ -z "$INPUT_COMMIT_MESSAGE" ]
+then
+  INPUT_COMMIT_MESSAGE="Update from https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
+fi
+
+echo "Adding git commit"
+git add .
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "$INPUT_COMMIT_MESSAGE"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$OUTPUT_BRANCH
+else
+  echo "No changes detected"
+fi
